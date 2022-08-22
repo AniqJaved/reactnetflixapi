1ac6f2007e39651865716570d4c0f195b3cd44b6
Aniq Javed <2aniqqjavedd4493@gmail.com>
2022-08-23 02:54:24 +0500

    User register router added

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-
+const authRoute = require('./routes/auth')
 
 //Grabbing port
 const port = process.env.PORT || 8800
 //Grabbing the enviroment variables
 dotenv.config()
 
+//Connecting with mongodb
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-.then(()=>console.log("DB Connection Successful"))
-.catch((err)=>console.log(err))
+}).then(()=>console.log("DB Connection Successful")).catch((err)=>console.log(err))
+
+
+//Express using json format for files
+app.use(express.json());
+
+//Path for auth router file
+app.use("/api/auth", authRoute);
 
 app.listen(port,()=>{
     console.log(`Server up at ` + port)
